27bb0e0bb830dde57c61e069da1ff6f149964cb1
SimiDrinkova <99390287+SimiDrinkova@users.noreply.github.com>
2025-02-15 14:04:00 +0100

    Ensure global FormData is defined only if not already present in Jest setup

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,7 @@
 import '@react-native-async-storage/async-storage/jest/async-storage-mock';
 
 // jest.setup.js
-global.FormData = class {
+// jest.setup.js
+global.FormData = global.FormData || class {
   append() {}
 };
